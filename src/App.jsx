f338302e650db2e21f1feaca4f80ef8e84e75a25
CosminDarius1 <83471623+CosminDarius1@users.noreply.github.com>
2023-12-28 19:37:53 +0000

    Managed to import data from the data.js file, and created a State Variable in order to Render the data on the browser

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,21 @@
+import data from '/src/data.js'
+import {useState} from "react";
+
 const App = () => {
-  return <h2>Birthday Reminder - Starter</h2>;
+    const [people, setPeople] = useState(data)
+
+  return (
+      <div>
+        {people.map((person) => {
+          const {id, name,age,image} = person
+            return (<div key={id}>
+                    <h1>{name}</h1>
+                    <h2>{age} years</h2>
+                    <img src={image} style={{width:300}}/>
+            </div>
+            )
+        })}
+      </div>
+  )
 };
 export default App;
