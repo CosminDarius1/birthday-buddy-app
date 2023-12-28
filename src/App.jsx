c03281be85cf76854764a690374872b7d7ffb263
CosminDarius1 <83471623+CosminDarius1@users.noreply.github.com>
2023-12-28 20:38:13 +0000

    Created List and Person Components, functionality is working, rendering the person from the data.js file, also added button which clears Person from the list when clicked

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,17 @@
 import data from '/src/data.js'
 import {useState} from "react";
+import List from "./Components/List.jsx";
 
 const App = () => {
     const [people, setPeople] = useState(data)
-
-  return (
-      <div>
-        {people.map((person) => {
-          const {id, name,age,image} = person
-            return (<div key={id}>
-                    <h1>{name}</h1>
-                    <h2>{age} years</h2>
-                    <img src={image} style={{width:300}}/>
-            </div>
-            )
-        })}
-      </div>
-  )
+   return (
+       <main>
+           <section className="container">
+               <h3>{people.length} birthdays today</h3>
+                <List people={people}/>
+                <button type='button' className=' btn btn-block' onClick={() => setPeople([])}>Clear List</button>
+           </section>
+       </main>
+   )
 };
 export default App;
